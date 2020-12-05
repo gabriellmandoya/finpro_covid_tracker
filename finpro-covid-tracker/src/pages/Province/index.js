b53192662c74a97af9764d5b61bf2d58cb046151
gabriellmandoya <s218110444@student.unklab.ac.id>
2020-12-05 21:10:42 +0700

    Add 3 Pages

---
+++ finpro-covid-tracker/src/pages/Province/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Province = () => {
+    return (
+        <div>
+            <p>P</p>
+        </div>
+    )
+}
+
+export default Province
---
+++ finpro-covid-tracker/src/pages/index.js
@@ -0,0 +1,5 @@
+import Global from "./Global";
+import Indonesia from "./Indonesia";
+import Province from "./Province";
+
+export { Global, Indonesia, Province };
