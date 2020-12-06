89615018019e3906c043c35f215d1ba68a2d2021
gabriellmandoya <s218110444@student.unklab.ac.id>
2020-12-06 17:05:41 +0700

    Style Province's Page

--- finpro-covid-tracker/src/components/Province_view.js
+++ finpro-covid-tracker/src/components/Province_view.js
@@ -1,13 +1,26 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, CardGroup } from "react-bootstrap";
+import '../App.css';
 
 const Province_view = ({provinsi, kasusPosi, kasusSemb, kasusMeni}) => {
     return (
-        <div>
-            <p>provinsi: {provinsi}</p>
-            <p>positif: {kasusPosi}</p>
-            <p>sembuh: {kasusSemb}</p>
-            <p>mati: {kasusMeni}</p>
-        </div>
+        <>
+        <CardGroup>
+            <Card className="view_data">
+                <Card.Text>{provinsi}</Card.Text>
+            </Card>
+            <Card className="view_data">
+                <Card.Text>{kasusPosi}</Card.Text>
+            </Card>
+            <Card className="view_data">
+                <Card.Text>{kasusSemb}</Card.Text>
+            </Card>
+            <Card className="view_data">
+                <Card.Text>{kasusMeni}</Card.Text>
+            </Card>  
+        </CardGroup>
+        </>
     );
 };
 
--- finpro-covid-tracker/src/pages/Province/index.js
+++ finpro-covid-tracker/src/pages/Province/index.js
@@ -1,6 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import Province_view from '../../components/Province_view';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container, CardGroup } from 'react-bootstrap';
+import '../../App.css';
 
 const Province = () => {
     const [datas, setDatas] = useState([]);
@@ -13,16 +16,32 @@ const Province = () => {
 
     return (
     <>
-      {datas.map((item) => {
-        return ( // eslint-disable-next-line
-          <Province_view 
-            provinsi={item.provinsi} 
-            kasusPosi={item.kasusPosi}
-            kasusSemb={item.kasusSemb}
-            kasusMeni={item.kasusMeni}
-          />
-        );
-      })}
+      <Container fluid>
+        <CardGroup>
+          <Card className="view_data">
+            <Card.Header>Province</Card.Header>
+          </Card>
+          <Card className="view_data">
+            <Card.Header>Positive</Card.Header>
+          </Card>
+          <Card className="view_data">
+            <Card.Header>Cured</Card.Header>
+          </Card>
+          <Card className="view_data">
+            <Card.Header>Death</Card.Header>
+          </Card>
+        </CardGroup>
+        {datas.map((item) => {
+          return ( // eslint-disable-next-line
+            <Province_view 
+              provinsi={item.provinsi} 
+              kasusPosi={item.kasusPosi}
+              kasusSemb={item.kasusSemb}
+              kasusMeni={item.kasusMeni}
+            />
+          );
+        })}
+      </Container>
     </>
     )
 }
