ff10e72db00fc1830958e09ea9022265370292d3
gabriellmandoya <s218110444@student.unklab.ac.id>
2020-12-06 14:53:47 +0700

    Style Indo's Page

--- finpro-covid-tracker/src/components/Global_view.js
+++ finpro-covid-tracker/src/components/Global_view.js
@@ -1,32 +1,34 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {CardDeck, Card} from 'react-bootstrap';
+import {CardDeck, Card, Container} from 'react-bootstrap';
 import '../App.css';
 
 const Global_view = ({confirmed, recovered, deaths}) => {
     return (
-        <div>
-            <CardDeck>
-                <Card className="view_data">
-                    <Card.Header>Confirmed</Card.Header>
-                    <Card.Body>
-                        <Card.Text>{confirmed}</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="view_data">
-                    <Card.Header>Cured</Card.Header>
-                    <Card.Body>
-                        <Card.Text>{recovered}</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="view_data">
-                    <Card.Header>Death</Card.Header>
-                    <Card.Body>
-                        <Card.Text>{deaths}</Card.Text>
-                    </Card.Body>
-                </Card>
-            </CardDeck>
-        </div>
+        <>
+            <Container fluid>
+                <CardDeck>
+                    <Card className="view_data">
+                        <Card.Header>Positive</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{confirmed}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="view_data">
+                        <Card.Header>Cured</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{recovered}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="view_data">
+                        <Card.Header>Death</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{deaths}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+            </Container>
+        </>
     );
 };
 
--- finpro-covid-tracker/src/components/Indo_view.js
+++ finpro-covid-tracker/src/components/Indo_view.js
@@ -1,12 +1,34 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {CardDeck, Card, Container} from 'react-bootstrap';
+import '../App.css';
 
 const Indo_view = ({positive, negative, death}) => {
     return (
-        <div>
-            <p>Positive: {positive}</p>
-            <p>Cured: {negative}</p>
-            <p>Death: {death}</p>
-        </div>
+        <>
+            <Container fluid>
+                <CardDeck>
+                    <Card className="view_data">
+                        <Card.Header>Positive</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{positive}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="view_data">
+                        <Card.Header>Cured</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{negative}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card className="view_data">
+                        <Card.Header>Death</Card.Header>
+                        <Card.Body>
+                            <Card.Text>{death}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+            </Container>
+        </>
     );
 };
 
